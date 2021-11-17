de21ce33475109f7430a7eb0455275702046af40
Harish <harishkrishna17@gmail.com>
2021-11-17 18:51:11 +0530

    jwt token fix

--- student.php
+++ student.php
@@ -7,23 +7,18 @@ $ROOM = "classroom-name";
 
 $key = "___APPSECRET___";
 $payload = array(
-    "aud" => "___APPID___",
-    "iss" => "___APPID___",
-    "sub" => "___JITSI_HOST___",
-    "exp" => time() + (24*60*60),
-    "room" => "classroom-name",
     "context" => array(
         "user" => array(
             "name" => "student-name",
             "email" => "student-name@___JITSI_HOST___",
             "avatar" => "https://gravatar.com/avatar/none.jpg",
-            "affiliation" => "student",
         ),
-        "features" => array(
-            "recording" => false,
-            "livestreaming" => false,
-            "screen-sharing" => true,
-        )
+    "aud" => "___APPID___",
+    "iss" => "___APPID___",
+    "sub" => "___JITSI_HOST___",
+    "exp" => time() + (24*60*60),
+    "room" => "classroom-name",
+    "moderator" => false
     )
 );
 
